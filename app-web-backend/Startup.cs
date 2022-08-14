2b983a82ec585a725fb2a4c8887f7d0ef1388b8c
Joao-Paulo-Faria <jp_faria@id.uff.br>
2022-08-14 20:06:41 -0300

    Alterações

---
+++ app-web-backend/Startup.cs
@@ -0,0 +1,53 @@
+﻿using app_web_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace app_web_backend
+{
+    public class Startup
+    {
+        private readonly object edpoints;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+            
+        }
+    }
+}
